55e59bfe014a846c646aa0fa1d862f0b5cd8db87
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-11-05 13:34:34 +0000

    workflows: buildx broken

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -8,15 +8,15 @@ if [ -z "$tag" ]; then
   tag='latest'
 fi
 
-docker buildx create --bootstrap --name mybuilder --platform $PLATFORM --use || echo 'skip'
+BUILDER=mybuilder
+docker buildx create --bootstrap --name $BUILDER --platform $PLATFORM --use
 
 path=$(dirname $DOCKERFILE)
 ext=${path##*/}
 image="ghcr.io/$GITHUB_REPOSITORY/$ext"
 
 docker buildx build \
-  --cache-from "type=local,src=$RUNNER_TEMP/docker-cache" \
-  --cache-to "type=local,dest=$RUNNER_TEMP/docker-cache-new" \
+  --builder $BUILDER \
   --push \
   --platform $PLATFORM \
   -t $image:$tag \
