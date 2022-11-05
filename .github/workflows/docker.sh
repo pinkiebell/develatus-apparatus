2bea23d41cb9dfb2bb8a17303f9158d07869ac84
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2022-11-05 13:24:27 +0000

    wip

--- .github/workflows/docker.sh
+++ .github/workflows/docker.sh
@@ -9,8 +9,7 @@ if [ -z "$tag" ]; then
 fi
 
 BUILDER=mybuilder
-docker buildx create --bootstrap --name $BUILDER --platform $PLATFORM --use || echo 'skip'
-docker buildx inspect
+docker buildx create --bootstrap --name $BUILDER --platform $PLATFORM --use
 
 path=$(dirname $DOCKERFILE)
 ext=${path##*/}
@@ -18,8 +17,6 @@ image="ghcr.io/$GITHUB_REPOSITORY/$ext"
 
 docker buildx build \
   --builder $BUILDER \
-  --cache-from "type=local,src=$RUNNER_TEMP/docker-cache" \
-  --cache-to "type=local,dest=$RUNNER_TEMP/docker-cache-new" \
   --push \
   --platform $PLATFORM \
   -t $image:$tag \
